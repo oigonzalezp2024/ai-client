00fd52f7ce8c1df216470036f76cd40e115e2a32
Oscar Gonzalez <oigonzalezp2024@gmail.com>
2025-06-04 17:43:31 -0500

    refactor(Usuarios): Mejorar encapsulamiento del módulo

--- chatbot_control/controlador/funciones_usuarios.js
+++ chatbot_control/controlador/funciones_usuarios.js
@@ -7,23 +7,11 @@ function agregardatos(id_usuario, nombre, celular, fecha_registro, activo, email
     "&email=" + email;
 
     accion = "insertar";
-    mensaje_si = "Usuario agregado con exito";
+    mensaje_si = "Usuario agregado exitosamente.";
     mensaje_no= "Error de registro";
     a_ajax(cadena, accion, mensaje_si, mensaje_no);
 }
-function agregardatosI(id_usuario, nombre, celular, fecha_registro, activo, email){
-    cadena = "id_usuario=" + id_usuario +
-    "&nombre=" + nombre +
-    "&celular=" + celular +
-    "&fecha_registro=" + fecha_registro +
-    "&activo=" + activo +
-    "&email=" + email;
 
-    accion = "insertar";
-    mensaje_si = "Usuario agregado con exito";
-    mensaje_no= "Error de registro";
-    a_ajax_i(cadena, accion, mensaje_si, mensaje_no);
-}
 function agregaform(datos) {
     d = datos.split('||');
     $('#id_usuariou').val(d[0]);
@@ -49,30 +37,11 @@ function modificarRegistro(){
     "&email=" + email;
 
     accion = "modificar";
-    mensaje_si = "Usuario modificado con exito";
+    mensaje_si = "Usuario modificado exitosamente.";
     mensaje_no= "Error de registro";
     a_ajax(cadena, accion, mensaje_si, mensaje_no);
 }
 
-function modificarRegistro(){
-    id_usuario = $('#id_usuariou').val();
-    nombre = $('#nombreu').val();
-    celular = $('#celularu').val();
-    fecha_registro = $('#fecha_registrou').val();
-    activo = $('#activou').val();
-    email = $('#emailu').val();
-    cadena = "id_usuario=" + id_usuario +
-    "&nombre=" + nombre +
-    "&celular=" + celular +
-    "&fecha_registro=" + fecha_registro +
-    "&activo=" + activo +
-    "&email=" + email;
-
-    accion = "modificar";
-    mensaje_si = "Usuario modificado con exito";
-    mensaje_no= "Error de registro";
-    a_ajax_i(cadena, accion, mensaje_si, mensaje_no);
-}
 
 function preguntarSiNo(id_usuario) {
     var opcion = confirm("¿Esta seguro de eliminar el registro?");
@@ -84,32 +53,13 @@ function preguntarSiNo(id_usuario) {
     }
 }
 
-function preguntarSiNoI(id_usuario) {
-    var opcion = confirm("¿Esta seguro de eliminar el registro?");
-    if (opcion == true) {
-        alert("El registro será eliminado.");
-        eliminarDatosI(id_usuario);
-    } else {
-        alert("El proceso de eliminación del registro ha sido cancelado.");
-    }
-}
-
 function eliminarDatos(id_usuario) {
     cadena = "id_usuario=" + id_usuario;
 
     accion = "borrar";
-    mensaje_si = "Usuario borrado con exito";
+    mensaje_si = "Usuario borrado exitosamente.";
     mensaje_no= "Error de registro";
     a_ajax(cadena, accion, mensaje_si, mensaje_no);
-}
-
-function eliminarDatosI(id_usuario) {
-    cadena = "id_usuario=" + id_usuario;
-
-    accion = "borrar";
-    mensaje_si = "Usuario borrado con exito";
-    mensaje_no= "Error de registro";
-    a_ajax_i(cadena, accion, mensaje_si, mensaje_no);
 }
 
 function a_ajax(cadena, accion, mensaje_si, mensaje_no){
@@ -120,22 +70,6 @@ function a_ajax(cadena, accion, mensaje_si, mensaje_no){
         success: function (r){
             if (r > 0) {
             $('#tabla').load('../vista/componentes/vista_usuarios.php');
-                alert(mensaje_si);
-            } else {
-                alert(mensaje_no);
-            }
-        }
-    });
-}
-
-function a_ajax_i(cadena, accion, mensaje_si, mensaje_no){
-    $.ajax({
-        type: "POST",
-        url: "../modelo/usuarios_modelo.php?accion="+accion,
-        data: cadena,
-        success: function (r){
-            if (r > 0) {
-            $('#tabla').load('../vista/componentes/vista_usuarios_id.php?id='+r);
                 alert(mensaje_si);
             } else {
                 alert(mensaje_no);
--- chatbot_control/vista/componentes/vista_usuarios.php
+++ chatbot_control/vista/componentes/vista_usuarios.php
@@ -1,90 +1,110 @@
 <?php
 include_once '../../modelo/conexion.php';
 $conn = conexion();
+
+class Usuarios
+{
+    private $conn;
+
+    public function __construct($conn = null)
+    {
+        $this->conn = $conn;
+    }
+
+    function findAll()
+    {
+        $conn = $this->conn;
+        $sql = 'SELECT * FROM usuarios ORDER BY id_usuario DESC';
+        $result = mysqli_query($conn, $sql);
+        $usuarios = [];
+        while ($usuario = mysqli_fetch_assoc($result)) {
+            array_push($usuarios, $usuario);
+        }
+        mysqli_close($conn);
+        return $usuarios;
+    }
+}
+
+$data = new Usuarios($conn);
+$rows = $data->findAll();
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <title>arreglos</title>
 </head>
 <div class="row"><br><br><br><br>
     <div>
-<center>
-<h2>Usuarios</h2>
-</center>
-<button class="btn navbar-left"
-               data-toggle="modal"
-               data-target="#modalNuevo">
-    <span class="glyphicon glyphicon-plus"></span>
-</button></div>
+        <center>
+            <h2>Usuarios</h2>
+        </center>
+        <button class="btn navbar-left"
+            data-toggle="modal"
+            data-target="#modalNuevo">
+            <span class="glyphicon glyphicon-plus"></span>
+        </button>
+    </div>
     <div class="table-responsive">
-    <table class="table table-hover table-condensed">
-    <thead>
-        <tr><th></th>
-            <th>id_usuario</th>
-            <th>nombre</th>
-            <th>celular</th>
-            <th>fecha_registro</th>
-            <th>activo</th>
-            <th>email</th>
-        <th></th></tr>
-   </thead>
-    <tbody>
-    <?php
-    $sql = 'SELECT * FROM usuarios ORDER BY id_usuario DESC';
-    $result = mysqli_query($conn, $sql);
-    WHILE($fila = mysqli_fetch_assoc($result)){
-        $datos = $fila['id_usuario'] . "||" .
-                  $fila['nombre'] . "||" .
-                  $fila['celular'] . "||" .
-                  $fila['fecha_registro'] . "||" .
-                  $fila['activo'] . "||" .
-                  $fila['email'];
-    ?>
-        <tr>
-            <td style="text-align:right;">
-                <a target="_blank" href="../pdf/pdff_usuarios_id.php"><i class="btn glyphicon glyphicon-download-alt"></i></a>
-                <a href="./usuarios_id.php?id=<?php echo $fila['id_usuario']; ?>"><i class="btn glyphicon glyphicon-eye-open"></i></a>
-                <button class="btn glyphicon glyphicon-pencil"
-                               data-toggle="modal"
-                               data-target="#modalEdicion"
-                               onclick="agregaform('<?php echo $datos; ?>')">
-                </button>
-            
-                <button class="btn glyphicon glyphicon-remove"
-                           onclick="preguntarSiNo('<?php echo $fila['id_usuario']; ?>')">
-                </button>
-</td>
-            <td><?php echo $fila['id_usuario']; ?></td>
-            <td><?php echo $fila['nombre']; ?></td>
-            <td><?php echo $fila['celular']; ?></td>
-            <td><?php echo $fila['fecha_registro']; ?></td>
-            <td><?php echo $fila['activo']; ?></td>
-            <td><?php echo $fila['email']; ?></td>
-            <td>
-                <a target="_blank" href="../pdf/pdff_usuarios_id.php"><i class="btn glyphicon glyphicon-download-alt"></i></a>
-                <a href="./usuarios_id.php?id=<?php echo $fila['id_usuario']; ?>"><i class="btn glyphicon glyphicon-eye-open"></i></a>
-                <button class="btn glyphicon glyphicon-pencil"
-                               data-toggle="modal"
-                               data-target="#modalEdicion"
-                               onclick="agregaform('<?php echo $datos; ?>')">
-                </button>
-            
-                <button class="btn glyphicon glyphicon-remove"
-                           onclick="preguntarSiNo('<?php echo $fila['id_usuario']; ?>')">
-                </button>
-</td>
-        </tr>
-    <?php
-    }
-    ?>
-    </tbody>
-    </table>
-<div>
-</div>
-</body>
+        <table class="table table-hover table-condensed">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>id_usuario</th>
+                    <th>nombre</th>
+                    <th>celular</th>
+                    <th>fecha_registro</th>
+                    <th>activo</th>
+                    <th>email</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($rows as $row) {
+                    $datos = $row['id_usuario'] . "||" .
+                        $row['nombre'] . "||" .
+                        $row['celular'] . "||" .
+                        $row['fecha_registro'] . "||" .
+                        $row['activo'] . "||" .
+                        $row['email'];
+                ?>
+                    <tr>
+                        <td style="text-align:right;">
+                            <button class="btn glyphicon glyphicon-pencil"
+                                data-toggle="modal"
+                                data-target="#modalEdicion"
+                                onclick="agregaform('<?php echo $datos; ?>')">
+                            </button>
+                            <button class="btn glyphicon glyphicon-remove"
+                                onclick="preguntarSiNo('<?php echo $row['id_usuario']; ?>')">
+                            </button>
+                        </td>
+                        <td><?php echo $row['id_usuario']; ?></td>
+                        <td><?php echo $row['nombre']; ?></td>
+                        <td><?php echo $row['celular']; ?></td>
+                        <td><?php echo $row['fecha_registro']; ?></td>
+                        <td><?php echo $row['activo']; ?></td>
+                        <td><?php echo $row['email']; ?></td>
+                        <td>
+                            <button class="btn glyphicon glyphicon-pencil"
+                                data-toggle="modal"
+                                data-target="#modalEdicion"
+                                onclick="agregaform('<?php echo $datos; ?>')">
+                            </button>
+                            <button class="btn glyphicon glyphicon-remove"
+                                onclick="preguntarSiNo('<?php echo $row['id_usuario']; ?>')">
+                            </button>
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+        <div>
+        </div>
+        </body>
+
 </html>
-<?php
-mysqli_close($conn);
-?>
