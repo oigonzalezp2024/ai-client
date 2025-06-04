3ccee10785d2d10c4cf16ca5a9dfad8d5c5864ae
Oscar Gonzalez <oigonzalezp2024@gmail.com>
2025-06-04 17:29:29 -0500

    refactor(preguntas): Mejorar encapsulamiento del módulo

--- chatbot_control/controlador/funciones_preguntas.js
+++ chatbot_control/controlador/funciones_preguntas.js
@@ -9,17 +9,7 @@ function agregardatos(id_pregunta, su_pregunta, respuesta, persona_id){
     mensaje_no= "Error de registro";
     a_ajax(cadena, accion, mensaje_si, mensaje_no);
 }
-function agregardatosI(id_pregunta, su_pregunta, respuesta, persona_id){
-    cadena = "id_pregunta=" + id_pregunta +
-    "&su_pregunta=" + su_pregunta +
-    "&respuesta=" + respuesta +
-    "&persona_id=" + persona_id;
 
-    accion = "insertar";
-    mensaje_si = "Pregunta agregada con exito";
-    mensaje_no= "Error de registro";
-    a_ajax_i(cadena, accion, mensaje_si, mensaje_no);
-}
 function agregaform(datos) {
     d = datos.split('||');
     $('#id_preguntau').val(d[0]);
@@ -44,37 +34,11 @@ function modificarRegistro(){
     a_ajax(cadena, accion, mensaje_si, mensaje_no);
 }
 
-function modificarRegistro(){
-    id_pregunta = $('#id_preguntau').val();
-    su_pregunta = $('#su_preguntau').val();
-    respuesta = $('#respuestau').val();
-    persona_id = $('#persona_idu').val();
-    cadena = "id_pregunta=" + id_pregunta +
-    "&su_pregunta=" + su_pregunta +
-    "&respuesta=" + respuesta +
-    "&persona_id=" + persona_id;
-
-    accion = "modificar";
-    mensaje_si = "Pregunta modificado con exito";
-    mensaje_no= "Error de registro";
-    a_ajax_i(cadena, accion, mensaje_si, mensaje_no);
-}
-
 function preguntarSiNo(id_pregunta) {
     var opcion = confirm("¿Esta seguro de eliminar el registro?");
     if (opcion == true) {
         alert("El registro será eliminado.");
         eliminarDatos(id_pregunta);
-    } else {
-        alert("El proceso de eliminación del registro ha sido cancelado.");
-    }
-}
-
-function preguntarSiNoI(id_pregunta) {
-    var opcion = confirm("¿Esta seguro de eliminar el registro?");
-    if (opcion == true) {
-        alert("El registro será eliminado.");
-        eliminarDatosI(id_pregunta);
     } else {
         alert("El proceso de eliminación del registro ha sido cancelado.");
     }
@@ -89,15 +53,6 @@ function eliminarDatos(id_pregunta) {
     a_ajax(cadena, accion, mensaje_si, mensaje_no);
 }
 
-function eliminarDatosI(id_pregunta) {
-    cadena = "id_pregunta=" + id_pregunta;
-
-    accion = "borrar";
-    mensaje_si = "Pregunta borrado con exito";
-    mensaje_no= "Error de registro";
-    a_ajax_i(cadena, accion, mensaje_si, mensaje_no);
-}
-
 function a_ajax(cadena, accion, mensaje_si, mensaje_no){
     $.ajax({
         type: "POST",
@@ -106,22 +61,6 @@ function a_ajax(cadena, accion, mensaje_si, mensaje_no){
         success: function (r){
             if (r > 0) {
             $('#tabla').load('../vista/componentes/vista_preguntas.php');
-                alert(mensaje_si);
-            } else {
-                alert(mensaje_no);
-            }
-        }
-    });
-}
-
-function a_ajax_i(cadena, accion, mensaje_si, mensaje_no){
-    $.ajax({
-        type: "POST",
-        url: "../modelo/preguntas_modelo.php?accion="+accion,
-        data: cadena,
-        success: function (r){
-            if (r > 0) {
-            $('#tabla').load('../vista/componentes/vista_preguntas_id.php?id='+r);
                 alert(mensaje_si);
             } else {
                 alert(mensaje_no);
--- chatbot_control/vista/componentes/vista_preguntas.php
+++ chatbot_control/vista/componentes/vista_preguntas.php
@@ -1,84 +1,106 @@
 <?php
 include_once '../../modelo/conexion.php';
 $conn = conexion();
+
+class Preguntas
+{
+    private $conn;
+
+    public function __construct($conn = null)
+    {
+        $this->conn = $conn;
+    }
+
+    function findAll()
+    {
+        $conn = $this->conn;
+        $sql = 'SELECT * FROM preguntas ORDER BY id_pregunta DESC';
+        $result = mysqli_query($conn, $sql);
+        $preguntas = [];
+        while ($pregunta = mysqli_fetch_assoc($result)) {
+            array_push($preguntas, $pregunta);
+        }
+        mysqli_close($conn);
+        return $preguntas;
+    }
+}
+
+$data = new Preguntas($conn);
+$rows = $data->findAll();
+
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <title>arreglos</title>
 </head>
 <div class="row"><br><br><br><br>
     <div>
-<center>
-<h2>Preguntas</h2>
-</center>
-<button class="btn navbar-left"
-               data-toggle="modal"
-               data-target="#modalNuevo">
-    <span class="glyphicon glyphicon-plus"></span>
-</button></div>
+        <center>
+            <h2>Preguntas</h2>
+        </center>
+        <button class="btn navbar-left"
+            data-toggle="modal"
+            data-target="#modalNuevo">
+            <span class="glyphicon glyphicon-plus"></span>
+        </button>
+    </div>
     <div class="table-responsive">
-    <table class="table table-hover table-condensed">
-    <thead>
-        <tr><th></th>
-            <th>id_pregunta</th>
-            <th>su_pregunta</th>
-            <th>respuesta</th>
-            <th>persona_id</th>
-        <th></th></tr>
-   </thead>
-    <tbody>
-    <?php
-    $sql = 'SELECT * FROM preguntas ORDER BY id_pregunta DESC';
-    $result = mysqli_query($conn, $sql);
-    WHILE($fila = mysqli_fetch_assoc($result)){
-        $datos = $fila['id_pregunta'] . "||" .
-                  $fila['su_pregunta'] . "||" .
-                  $fila['respuesta'] . "||" .
-                  $fila['persona_id'];
-    ?>
-        <tr>
-            <td style="text-align:right;">
-                <a target="_blank" href="../pdf/pdff_preguntas_id.php"><i class="btn glyphicon glyphicon-download-alt"></i></a>
-                <a href="./preguntas_id.php?id=<?php echo $fila['id_pregunta']; ?>"><i class="btn glyphicon glyphicon-eye-open"></i></a>
-                <button class="btn glyphicon glyphicon-pencil"
-                               data-toggle="modal"
-                               data-target="#modalEdicion"
-                               onclick="agregaform('<?php echo $datos; ?>')">
-                </button>
-            
-                <button class="btn glyphicon glyphicon-remove"
-                           onclick="preguntarSiNo('<?php echo $fila['id_pregunta']; ?>')">
-                </button>
-</td>
-            <td><?php echo $fila['id_pregunta']; ?></td>
-            <td><?php echo $fila['su_pregunta']; ?></td>
-            <td><?php echo $fila['respuesta']; ?></td>
-            <td><?php echo $fila['persona_id']; ?></td>
-            <td>
-                <a target="_blank" href="../pdf/pdff_preguntas_id.php"><i class="btn glyphicon glyphicon-download-alt"></i></a>
-                <a href="./preguntas_id.php?id=<?php echo $fila['id_pregunta']; ?>"><i class="btn glyphicon glyphicon-eye-open"></i></a>
-                <button class="btn glyphicon glyphicon-pencil"
-                               data-toggle="modal"
-                               data-target="#modalEdicion"
-                               onclick="agregaform('<?php echo $datos; ?>')">
-                </button>
-            
-                <button class="btn glyphicon glyphicon-remove"
-                           onclick="preguntarSiNo('<?php echo $fila['id_pregunta']; ?>')">
-                </button>
-</td>
-        </tr>
-    <?php
-    }
-    ?>
-    </tbody>
-    </table>
-<div>
-</div>
-</body>
+        <table class="table table-hover table-condensed">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>id_pregunta</th>
+                    <th>su_pregunta</th>
+                    <th>respuesta</th>
+                    <th>persona_id</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($rows as $row) {
+                    $datos = $row['id_pregunta'] . "||" .
+                        $row['su_pregunta'] . "||" .
+                        $row['respuesta'] . "||" .
+                        $row['persona_id'];
+                ?>
+                    <tr>
+                        <td style="text-align:right;">
+                            <button class="btn glyphicon glyphicon-pencil"
+                                data-toggle="modal"
+                                data-target="#modalEdicion"
+                                onclick="agregaform('<?php echo $datos; ?>')">
+                            </button>
+
+                            <button class="btn glyphicon glyphicon-remove"
+                                onclick="preguntarSiNo('<?php echo $row['id_pregunta']; ?>')">
+                            </button>
+                        </td>
+                        <td><?php echo $row['id_pregunta']; ?></td>
+                        <td><?php echo $row['su_pregunta']; ?></td>
+                        <td><?php echo $row['respuesta']; ?></td>
+                        <td><?php echo $row['persona_id']; ?></td>
+                        <td>
+                            <button class="btn glyphicon glyphicon-pencil"
+                                data-toggle="modal"
+                                data-target="#modalEdicion"
+                                onclick="agregaform('<?php echo $datos; ?>')">
+                            </button>
+                            <button class="btn glyphicon glyphicon-remove"
+                                onclick="preguntarSiNo('<?php echo $row['id_pregunta']; ?>')">
+                            </button>
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+        <div>
+        </div>
+        </body>
+
 </html>
-<?php
-mysqli_close($conn);
-?>
